317c64255f17e976bda12e487fb116fc5c31b3ad
danielyordanov07 <98211151+danielyordanov07@users.noreply.github.com>
2025-03-10 09:37:36 +0200

    look left

---
+++ components/item_page.js
@@ -0,0 +1,36 @@
+//Carousel
+const gallery = [
+    { img: "best/earrings.jpg"},
+    { img: "best/earrings.jpg"},
+    { img: "best/earrings.jpg"},
+    { img: "best/earrings.jpg"},
+];
+
+
+
+// Quantity input
+document.addEventListener("DOMContentLoaded", function () {
+    const quantityInput = document.getElementById("quantity-input");
+    const quantityMinus = document.getElementById("quantity-minus");
+    const quantityPlus = document.getElementById("quantity-plus");
+
+    quantityInput.value = quantityInput.value || 1;
+
+    quantityMinus.addEventListener("click", function () {
+        let currentValue = parseInt(quantityInput.value, 10) || 1;
+        if (currentValue > 1) {
+            quantityInput.value = currentValue - 1;
+        }
+    });
+
+    quantityPlus.addEventListener("click", function () {
+        let currentValue = parseInt(quantityInput.value, 10) || 1;
+        quantityInput.value = currentValue + 1;
+    });
+
+    quantityInput.addEventListener("input", function () {
+        if (quantityInput.value < 1 || isNaN(quantityInput.value)) {
+            quantityInput.value = 1;
+        }
+    });
+});
